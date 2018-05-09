967d9d66bb17eb05bcca27df920168595dddd711
A74696 <josediogobastos@gmail.com>
2018-05-09 17:40:05 +0100

    compact feito

--- trabalho.py
+++ trabalho.py
@@ -102,46 +102,116 @@ def ex1():
 	print("Inserções - "+str(ad))
 	print("--------------------------")
 
-
+'''
 def compact(self):
-	tam = len(self.nodes.keys())-1
+	tam = len(self.nodes.keys())-1 #13
 	pos = 2
+	apagados = 0
 	string = ""
 	while(pos!=tam):
 		if(len(self.nodes[pos][1])==1):
-			ini = list(self.nodes.keys())[pos]
-			#print("ini "+str(ini))
+			ini = list(self.nodes.keys())[pos] - apagados
+			print("ini "+str(ini))
 			apagar = []
 			while(self.nodes[pos][0]<0):
 				if(self.nodes[pos][0]<0 and pos!=ini):
 					apagar.append(pos)
-					#print(apagar)
+					print(apagar)
 				string+=(list(self.nodes[pos][1].keys())[0])
 				pos = list(self.nodes[pos][1].values())[0]
-				#print("pos"+str(pos))
-				#print(string)
-				#print(self.nodes[pos][0])
+				print("pos"+str(pos))
+				print(string)
+				print(self.nodes[pos][0])
 			self.nodes[ini][1][string] = pos
 			self.nodes[ini][1].pop(string[0],None)
 			for i in apagar:
-				#print(i)
 				self.nodes.pop(i,None)
-			#print(self.nodes[ini][1])
+				apagados += 1
+			print(self.nodes[ini][1])
 			print(self.nodes)
+			string = []
+			pos+=1
 		else:
 			#for k in self.nodes[pos][1].values():
 			pos = 13
 			#pos = 9
 
-	
+
+def test(self):
+	apagados = 0
+	string = ""
+
+	for i in self.nodes.keys():
+		for k,v in self.nodes[i][1].items():
+			symbol = k
+			pos = v
+			seg = (self.nodes[pos][1])
+			print(pos)
+			if len(seg)==1:
+				ini = list(self.nodes.keys())[pos] - apagados
+				print("ini "+str(ini))
+				apagar = []
+				while(self.nodes[pos][0]<0):
+					if(self.nodes[pos][0]<0 and pos!=ini):
+						apagar.append(pos)
+						print(apagar)
+					string+=(list(self.nodes[pos][1].keys())[0])
+					pos = list(self.nodes[pos][1].values())[0]
+					print("pos"+str(pos))
+					print(string)
+					print(self.nodes[pos][0])
+				self.nodes[ini][1][string] = pos
+				self.nodes[ini][1].pop(string[0],None)
+				for i in apagar:
+					self.nodes.pop(i,None)
+					apagados += 1
+				print(self.nodes[ini][1])
+				print(self.nodes)
+				string = []
+				pos+=1
+			else:
+				print("NNN")
+'''
+def compactaux(self,x):
+	apagados = 0
+	it = 0
+	for k,v in self.nodes[x][1].items():
+		it+=1
+		string = k
+		pos = v
+		seg = (self.nodes[pos][1])
+		if len(seg)==1:
+			apagar = []
+			while(self.nodes[pos][0]<0):
+				if(self.nodes[pos][0]<0 and pos!=x):
+					apagar.append(pos)
+				string+=(list(self.nodes[pos][1].keys())[0])
+				pos = list(self.nodes[pos][1].values())[0]
+			self.nodes[x][1][string] = pos
+			self.nodes[x][1].pop(string[0],None)
+			for i in apagar:
+				self.nodes.pop(i,None)
+				apagados += 1
+		else:
+			compactaux(self,pos)
+		
+def compact(self):
+	print("----------------------------------------------------------")
+	print("		ÁRVORE ANTES DA COMPACTAÇÃO")
+	print("----------------------------------------------------------")
+	self.print_tree()
+	compactaux(self,0)
+	print("----------------------------------------------------------")
+	print("		ÁRVORE DEPOIS DA COMPACTAÇÃO")
+	print("----------------------------------------------------------")
+	self.print_tree()
 
 def ex2():
 	
-	seq = "ATAA"
+	seq = "ATGAAC"
 	st = SuffixTrie()
 	st.suffixTrieFromSeq(seq)
-	st.print_tree()
-	ct = compact(st)
+	sim = compact(st)
 
 if __name__=="__main__":
 
