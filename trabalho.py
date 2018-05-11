2f721612dca65fc062041ffc0209b461b8fcb5a8
A74696 <josediogobastos@gmail.com>
2018-05-11 16:14:11 +0100

    documentação

--- trabalho.py
+++ trabalho.py
@@ -4,12 +4,12 @@ import numpy as np
 import plotly.plotly as py
 from pattern_search import Trie, SuffixTrie
 
-allmutations = {}
+allmutations = {} #dicionário global que contém todas as mutações
 
 def getVariants(fileName):
 	data = allel.read_vcf(fileName)
-	refs = data['variants/REF']
-	alts = data['variants/ALT']
+	refs = data['variants/REF'] #obter lista das sequências de referência
+	alts = data['variants/ALT'] #obter lista das sequências alternativas
 
 	return refs,alts
 
@@ -17,43 +17,41 @@ def perg_a(fileName):
 
 	refs,alts = getVariants(fileName)
 
-	for r,a in zip(refs,alts):
+	for r,a in zip(refs,alts): #para cada elemento em cada uma das listas
 		mutation = r+'>'+a[0]
-		if mutation not in allmutations.keys():
-			allmutations[mutation] = 1
-		else:
+		if mutation not in allmutations.keys(): #se mutação não existe no dicionário, mete o valor a 1
+			allmutations[mutation] = 1 
+		else: 									#se já existe incrementa o valor
 			allmutations[mutation]+=1
-		if a[1]!="": 
+		if a[1]!="": 							#se existir outro sequência alternativa faz o mesmo
 			mutation = r+'>'+a[1]
 			if mutation not in allmutations.keys():
 				allmutations[mutation] = 1
 			else:
 				allmutations[mutation]+=1
 
-	return(str(len(allmutations)))
-	#for m in (sorted(allmutations, key=allmutations.get,reverse = True)):
-	#	print (m+" "+str(allmutations[m]))
+	return(str(len(allmutations))) #retorna o tamanho do dicionário
 
 def perg_b(fileName):
 
 	refs,alts = getVariants(fileName)
 
-	snp = {}
+	snp = {} #dicionário que contem as SNPs e a quantas vezes aparecem
 
-	for mut in allmutations.keys():
-		if len(mut)==3:
-			snp[mut] = allmutations[mut]
+	for mut in allmutations.keys(): #para cada chave do dicionário
+		if len(mut)==3:             #se o seu tamanho é 3 significa que é SNP
+			snp[mut] = allmutations[mut] #logo, adiciona no dicionário de snps, o número de mutações dessa SNP
 
-	labels = snp.keys()
-	exp = snp.values()
-	numb = list(snp)
+	labels = snp.keys() #eixo do x representado pelas chaves do dicionário
+	exp = snp.values() #eixo do y representado pelos valores do dicionário
+	numb = list(snp) 
 	f = plt.figure()
-	plt.bar(numb,exp, align = 'center')
+	plt.bar(numb,exp, align = 'center') #criação do plot
 	plt.title("Mutações SNP")
 	plt.xticks(numb, labels);
 	plt.xlabel("Mutação")
 	plt.ylabel("Ocorrências")
-	f.savefig("snp.pdf", bbox_inches='tight')
+	f.savefig("snp.pdf", bbox_inches='tight') #guarda o gráfico num ficheiro pdf na diretoria atual
 
 def perg_c(fileName):
 
@@ -61,14 +59,14 @@ def perg_c(fileName):
 
 	snp=de=ad=0
 	for r,a in zip(refs,alts):
-		tamref = len(r)
-		tamalt0 = len(a[0])
-		if tamref==tamalt0:
+		tamref = len(r) #tamanho da seq de referência
+		tamalt0 = len(a[0]) #tamanho da primeira seq alternativa
+		if tamref==tamalt0:  #se for igual é uma snp
 			snp+=1
-		elif tamref>tamalt0:
+		elif tamref>tamalt0: #se o tamanho da seq de referência, foi uma deleção
 			de+=1
-		else: ad+=1
-		if a[1]!="":
+		else: ad+=1 #se não foi uma inserção
+		if a[1]!="": #fazer o mesmo para a segunda sequência se esta não for vazia
 			tamalt1 = len(a[1])
 			if tamref==tamalt1:
 				snp+=1
@@ -104,21 +102,21 @@ def ex1():
 
 
 def compactaux(self,x):
-	for string,pos in self.nodes[x][1].items():
+	for string,pos in self.nodes[x][1].items(): #para cada key,value do segundo elemento do tuplo do nodo x
 		seg = (self.nodes[pos][1])
-		if len(seg)==1:
+		if len(seg)==1: #se o nodo contém apenas um elemento
 			apagar = []
-			while(self.nodes[pos][0]<0 and len(self.nodes[pos][1].values())==1):
+			while(self.nodes[pos][0]<0 and len(self.nodes[pos][1].values())==1): #enquanto nodo não for terminal e contiver apenas um elemento
 				if(self.nodes[pos][0]<0 and pos!=x):
-					apagar.append(pos)
-				string+=(list(self.nodes[pos][1].keys())[0])
-				pos = list(self.nodes[pos][1].values())[0]
-			self.nodes[x][1][string] = pos
-			self.nodes[x][1].pop(string[0],None)
-			for i in apagar:
+					apagar.append(pos)  #adicionar nodo à lista para apagar
+				string+=(list(self.nodes[pos][1].keys())[0]) #retirar a key a adicionar à string
+				pos = list(self.nodes[pos][1].values())[0] #retirar a posição
+			self.nodes[x][1][string] = pos #atualizar o nodo inicial com a string e posição retirada
+			self.nodes[x][1].pop(string[0],None) #apagar o elemento a mais
+			for i in apagar: #apagar os nodos a mais da árvore
 				self.nodes.pop(i,None)
 		else:
-			compactaux(self,pos)
+			compactaux(self,pos) #se não tiver só um elemento fazer recursividade na árvore
 		
 def compact(self):
 	print("----------------------------------------------------------")
@@ -137,36 +135,36 @@ def findPattern(self, pattern):
     outrpos = 0
     while (pos != (len(pattern))):
         string = ""
-        primelems = list(k[0] for k in list(self.nodes[node][1].keys()))
-        elems = list(k for k in list(self.nodes[node][1].keys()))
+        primelems = list(k[0] for k in list(self.nodes[node][1].keys())) #lista com os primeiros carateres dos elementos do nodo
+        elems = list(k for k in list(self.nodes[node][1].keys())) #lista com os elementos do nodo
 
-        if pattern[pos] in primelems:
-            indice = primelems.index(pattern[pos])
-            string+=pattern[pos]
+        if pattern[pos] in primelems: #se o carater do padrão está na lista dos primeiros elementos
+            indice = primelems.index(pattern[pos]) #retirar o índice em que está contido na lista
+            string+=pattern[pos] #adicionar o carater à string
             outrpos = 0
 
-            if len(elems[indice])==1:
+            if len(elems[indice])==1: #se o tamanho for apenas de 1
 
                 node = self.nodes[node][1][pattern[pos]]
                 pos += 1
                 
 
             else:
-                while(elems[indice][outrpos]==pattern[pos] and pos != len(elems[indice])-1 and pos != len(pattern)-1):
-
+                while(elems[indice][outrpos]==pattern[pos] and pos != len(elems[indice])-1 and pos != len(pattern)-1): 
+                	#enquanto o padrão estiver igual à string do índice pretendido do nodo e enquanto houver padrão/string
                     pos += 1
                     outrpos += 1
                     string+=elems[indice][pos]
                     
-                if(elems[indice][outrpos]!=pattern[pos]):
+                if(elems[indice][outrpos]!=pattern[pos]): #se for diferente retornar None
                     return None
 
-                elif(pos==len(pattern)-1):
+                elif(pos==len(pattern)-1): #se acabar aí retirar o próximo nodo
                     pos += 1
                     outrpos += 1
                     node = self.nodes[node][1][elems[indice]]
                     
-                else:
+                else: #fazer o mesmo para o próximo nodo
                     newpos = 0
                     if string in primelems:
                         ind = primelems.index(string)
@@ -180,7 +178,7 @@ def findPattern(self, pattern):
             return None
     return getLeafesBelow(self,node)
 
-def getLeafesBelow(self, node):
+def getLeafesBelow(self, node): #mesma função do pattern_search.py
         res = []
         if self.nodes[node][0] >= 0:
         	res.append(self.nodes[node][0])
@@ -194,11 +192,11 @@ def getLeafesBelow(self, node):
 def repeats(st,k,ocs):
 	pos = 0
 	res = []
-	seq = getSeq(st,0,"")
-	for pos in range(len(seq)-k+1):
+	seq = getSeq(st,0,"") #função para obter a sequência original
+	for pos in range(len(seq)-k+1): #técnica de sliding-window
 		pat = seq[pos:pos+k]
 		find = findPattern(st,pat)
-		if find!=None and len(find) >= ocs:
+		if find!=None and len(find) >= ocs: #Se no uso da função findPattern o output não for vazio e for maior que ocs, adicionar a lista de return
 			res.append(pat)
 	if res:
 		return set(res)
@@ -207,17 +205,17 @@ def repeats(st,k,ocs):
 
 
 def getSeq(self,node,string):
-    if self.nodes[node][0] < 0:
-        valores = list(self.nodes[node][1].values())
-        v = min(valores)
+    if self.nodes[node][0] < 0: #verificar se não é nodo terminal
+        valores = list(self.nodes[node][1].values()) #lista dos valores do segundo elemento do tuplo
+        v = min(valores) #obter o menor valor da lista
         for ke,va in list(self.nodes[node][1].items()):
         	if va==v:
         		k=ke
-        string += k
-        newnode = self.nodes[node][1][k]
-        return getSeq(self,newnode,string)
+        string += k #fazer append da chave à string
+        newnode = self.nodes[node][1][k] #declaração doo próximo nodo
+        return getSeq(self,newnode,string) #recursividade para executar a função para o próximo nodo, com a string calculada até ao momento
     else:
-        return string[:-1]
+        return string[:-1] #eliminar o '$'
 
 
 def ex2():
